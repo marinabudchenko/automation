e3753797873fb3980de4c2421d6c709ebd81441e
Маринабудченко <marinabudcenko923@gmail.com>
2024-10-04 18:30:35 +0300

    first homev

---
+++ Lesson6/lesson_6_task_2.py
@@ -0,0 +1,28 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("http://uitestingplayground.com/textinput")
+
+#указать в поле текст SkyPro
+input_field = driver.find_element(By.CSS_SELECTOR, "#newButtonName").send_keys("SkyPro")
+#нажать на синюю кнопку
+driver.find_element(By.CSS_SELECTOR, "#updatingButton").click
+#получить текст кнопки
+button_text = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located
+    ((By.CSS_SELECTOR, "btn-primary"))).text
+#вывести в консоль скайпро
+print(button_text)
+
+driver.quit()
+
+
+
